ae4f39e6ba1546f9dd93074a9d757823e9e6dd79
LUCIANO131231 <2018110461@udh.edu.pe>
2025-02-26 11:04:23 -0500

    Eliminar archivo CSS no utilizado y agregar componente Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-900 text-white py-12">
+      <div className="container mx-auto px-6">
+        <div className="flex justify-center">
+          <div className="text-center text-gray-400">
+            <p>Copyright @2024, <a href="#">Luciano</a> All Rights Reserved.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
