ab626822139421d040e975e15e832e54003d574b
LUCIANO131231 <2018110461@udh.edu.pe>
2025-03-01 00:24:46 -0500

    Agregar archivos de experiencia educativa y laboral

---
+++ src/utils/EducationExperience.jsx
@@ -0,0 +1,20 @@
+export const educationExperience = [
+  {
+    id: 1,
+    years: "2024 - 2025",
+    institute: "Bachelor Ingenieria de Sistemas e Infórmatica",
+    position: "Univerisdad de Huánuco"
+  },
+  {
+    id: 2,
+    years: "2024 - 2025",
+    institute: "Bachelor Ingenieria de Sistemas e Infórmatica",
+    position: "Univerisdad de Huánuco"
+  },
+  {
+    id: 3,
+    years: "2024 - 2025",
+    institute: "Bachelor Ingenieria de Sistemas e Infórmatica",
+    position: "Univerisdad de Huánuco"
+  },
+]
