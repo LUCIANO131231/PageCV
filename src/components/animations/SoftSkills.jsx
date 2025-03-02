ae8c28e0f6c28db43cb8c1704e934da05ca6cf4b
LUCIANO131231 <2018110461@udh.edu.pe>
2025-03-02 18:52:12 -0500

    Agregue el componente SoftSkills con habilidades predefinidas e íconos para una mejor visualización

---
+++ src/components/animations/SoftSkills.jsx
@@ -0,0 +1,48 @@
+import {RiTeamFill, RiLightbulbFill, RiTimeFill} from '@remixicon/react';
+
+const softSkills = [
+  {
+    id: 1,
+    name: "Trabajo en equipo",
+    level: 90,
+    icon: <RiTeamFill size={40} />,
+    color: "bg-blue-500"
+  },
+  {
+    id: 2,
+    name: "Liderazgo",
+    level: 85,
+    icon: <RiLightbulbFill size={40} />,
+    color: "bg-purple-500"
+  },
+  {
+    id: 3,
+    name: "Gestión del tiempo",
+    level: 80,
+    icon: <RiTimeFill size={40} />,
+    color: "bg-green-500"
+  },
+  {
+    id: 4,
+    name: "Resolución de problemas",
+    level: 95,
+    icon: <RiLightbulbFill size={40} />,
+    color: "bg-yellow-500"
+  },
+  {
+    id: 5,
+    name: "Comunicación",
+    level: 88,
+    icon: <RiTeamFill size={40} />,
+    color: "bg-red-500"
+  },
+  {
+    id: 6,
+    name: "Adaptabilidad",
+    level: 92,
+    icon: <RiTimeFill size={40} />,
+    color: "bg-indigo-500"
+  }
+];
+
+export default softSkills;
