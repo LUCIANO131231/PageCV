bbab2c5413a33175c92c6150d9cad57a282a6372
LUCIANO131231 <2018110461@udh.edu.pe>
2025-03-01 22:58:00 -0500

    Refactor education and work experience data structure by replacing JSX files with JS files for improved clarity and maintainability

---
+++ src/utils/EducationExperience.js
@@ -0,0 +1,14 @@
+export const educationExperience = [
+  {
+    id: 1,
+    years: "2025",
+    institute: "Bachiller en Ingenieria de Sistemas e Infórmatica",
+    position: "Universidad de Huánuco"
+  },
+  {
+    id: 2,
+    years: "2017",
+    institute: "Educación Secundaria Completa",
+    position: "La Divina Misericordia"
+  }
+]
---
+++ src/utils/WorkExperience.js
@@ -0,0 +1,14 @@
+export const workExperience = [
+  {
+    id: 1,
+    years: "2024 - 2025",
+    institute: "Universidad de Huánuco",
+    position: "Desarrollo Frontend"
+  },
+  {
+    id: 2,
+    years: "2022",
+    institute: "Registro Nacional de Identificación y Estado Civil",
+    position: "Servicio de Registrador Oficina"
+  }
+]
