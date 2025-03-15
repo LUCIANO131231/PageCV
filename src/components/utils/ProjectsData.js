8a27e1214b3d9dbb4837c08c6dea1ce139dce843
LUCIANO131231 <2018110461@udh.edu.pe>
2025-03-15 11:16:29 -0500

    Agregar archivo ProjectsData.js con datos de proyectos y sus respectivas imágenes

---
+++ src/components/utils/ProjectsData.js
@@ -0,0 +1,43 @@
+import project1 from "../../assets/images/projects/1.png"
+import project2 from "../../assets/images/projects/2.jpg"
+import project3 from "../../assets/images/projects/1.png"
+import project4 from "../../assets/images/projects/1.png"
+
+export const projectsData = [
+  {
+    id: 1,
+    src: project1,
+    category: "Design",
+    title: "Titulo de mi proyecto"
+  },
+  {
+    id: 2,
+    src: project2,
+    category: "Game",
+    title: "Titulo de mi proyecto"
+  },
+  {
+    id: 3,
+    src: project4,
+    category: "Branding", 
+    title: "Titulo de mi proyecto"
+  },
+  {
+    id: 4,
+    src: project1,
+    category: "Marketing",
+    title: "Titulo de mi proyecto"
+  },
+  {
+    id: 5,
+    src: project2,
+    category: "Design",
+    title: "Titulo de mi proyecto"
+  },
+  {
+    id: 6,
+    src: project3,
+    category: "Marketing",
+    title: "Titulo de mi proyecto"
+  }
+]
