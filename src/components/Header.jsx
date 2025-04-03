f1883d87660d9539d043339cab3637d377ddcaee
LUCIANO131231 <2018110461@udh.edu.pe>
2025-04-03 00:39:38 -0500

    Actualizar estilos y estructura del componente Header, incluyendo cambios en el logo, menú de navegación y mejoras en la responsividad.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
 import {useEffect, useState} from "react"
 import {Link} from "react-scroll"
-import logo from "../assets/images/robot.png"
+import logo from "../assets/images/about/dibujo.svg"
+import { RiMenuFill } from '@remixicon/react';
 
 const menuList = [
   { id: 1, path: "home", label: "Home" },
   { id: 2, path: "about", label: "About" },
   { id: 3, path: "resume", label: "Resume" },
   { id: 4, path: "skill", label: "Skills" },
-  { id: 5, path: "project", label: "Projects" },
+  // { id: 5, path: "project", label: "Projects" },
   { id: 6, path: "certificate", label: "Certificates" },
-  { id: 7, path: "contact", label: "Contact" },
+  // { id: 7, path: "contact", label: "Contact" },
 ]
 
 const Header = () => {
@@ -27,30 +28,34 @@ const Header = () => {
   };
 
   return (
-    <header className={`w-full top-0 left-0 z-50 transition-all duration-300 ${isSticky ? "fixed bg-white shadow-lg" : "absolute bg-transparent"}`}>
+    <header className={`w-full top-0 left-0 z-50 transition-all duration-300 ${isSticky ? "fixed bg-white/95 shadow-lg" : "absolute bg-transparent"}`}>
       <div className="w-full py-4 px-6">
         <div className="max-w-7xl mx-auto flex justify-between items-center">
           
           {/*logo personal*/}
           <div className="flex items-center">
             <a href="#">
-              <img src={logo} alt="Logo" title="Logo" className="w-20" />
+              <img src={logo} alt="Logo" title="Logo" className="w-36 object-contain animate-pulse" />
             </a>
           </div>
 
           {/*navbar*/}
-          <nav className="hidden lg:flex">
+          <nav className="hidden lg:flex font-glori text-lg">
             <ul className="flex space-x-6">
               {menuList.map(({ id, label, path }) => (
-                <li key={id}>
+                <li key={id} className="relative hover:rotate-6">
                   <Link 
                     to={path} 
                     spy={true} 
                     smooth={true} 
                     offset={0} 
                     duration={500} 
-                    className="cursor-pointer text-gray-700 hover:text-black transition duration-200">
+                    className="cursor-pointer relative py-2 px-1 text-gray-900 transition-all duration-300 group">
                     {label}
+                    
+                    {/* Efecto hover: línea que crece */}
+                    <span className="absolute left-0 bottom-0 w-0 h-1 bg-black group-hover:w-full transition-all duration-700"></span>
+                    
                   </Link>
                 </li>
               ))}
@@ -59,27 +64,27 @@ const Header = () => {
           
           {/*para movil*/}
           <button onClick={() => setMenuOpen(!menuOpen)} className="lg:hidden flex flex-col space-y-1 focus:outline-none">
-            <span className="block w-6 h-0.5 bg-black"></span>
-            <span className="block w-6 h-0.5 bg-black"></span>
-            <span className="block w-6 h-0.5 bg-black"></span>
+          <RiMenuFill className="text-gray-700" size={20} />
           </button>
         </div>
       </div>
 
       {/* Mobile Menu */}
       {menuOpen && (
-        <div className="lg:hidden bg-white shadow-md">
-          <ul className="flex flex-col items-center py-4">
+        <div 
+          className={`lg:hidden bg-white/95 shadow-lg overflow-hidden transition-all duration-300 ease-in-out 
+          ${menuOpen ? "max-h-96" : "max-h-0"}`}>
+          <ul className="flex flex-col items-center py-4 w-full">
             {menuList.map(({ id, label, path }) => (
-              <li key={id} className="py-2">
+              <li key={id} className="py-2 w-full">
                 <Link
                   to={path}
                   spy={true}
                   smooth={true}
                   offset={0}
                   duration={500}
-                  className="cursor-pointer text-gray-700 hover:text-black transition duration-200"
-                  onClick={() => setMenuOpen(false)}>
+                  className="block py-2 px-4 text-center transition duration-200 cursor-pointer text-gray-700 hover:bg-gray-300 hover:rounded-4xl"
+                  onClick={() => {setMenuOpen(false);}}>
                   {label}
                 </Link>
               </li>
