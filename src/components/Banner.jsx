444e21b8774db57fbd228d9dde60cb24db0c2297
LUCIANO131231 <lucianorenzo487@gmail.com>
2025-08-31 22:53:12 -0500

    Update component styles and text content

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,16 +4,16 @@ const Banner = () => {
   return (
     <section id="home" className="py-72 sm:py-60">
       <div className="max-w-7xl mx-auto px-6">
-        <div className="bg-[#E2E2B6] shadow-2xl rounded-2xl p-10 text-center">
+        <div className="bg-[#DEE8CE] shadow-2xl rounded-2xl p-10 text-center">
           <div className="flex flex-col items-center">
             <div className="w-full">
               <SlideUp>
                 <div className="text-center">
-                  <h1 className="text-[110px] font-slowin capitalize leading-[68px] text-[#D98324] text-shadow">
-                    I&#8217;m <span className="letra text-white">Luciano</span> Estela
+                  <h1 className="text-[110px] font-slowin capitalize leading-[68px] text-[#7ADAA5] text-shadow">
+                    I'm <span className="letra text-white">Luciano</span> Estela
                   </h1>
-                  <div className="inline-block bg-white px-10 py-4 mt-8 font-glori rounded-4xl text-2xl text-gray-500">
-                    <span>UI Designer and Frontend Developer</span>
+                  <div className="inline-block bg-white px-10 py-4 mt-8 font-glori rounded-4xl text-2xl">
+                    <span>UI/UX Designer and Frontend Developer</span>
                   </div>
                 </div>
               </SlideUp>
