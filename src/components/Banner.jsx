2ca6b0a3aaffa0afc69ec80d7ae84873d37e1061
LUCIANO131231 <lucianorenzo487@gmail.com>
2025-09-03 10:26:26 -0500

    Add SplitText component for animated text display in Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,5 @@
 import SlideUp from "./animations/Slideup"
+import SplitText from "./animations/SplitText"
 
 const Banner = () => {
   return (
@@ -12,9 +13,21 @@ const Banner = () => {
                   <h1 className="text-[110px] font-slowin capitalize leading-[68px] text-[#7ADAA5] text-shadow">
                     I'm <span className="letra text-white">Luciano</span> Estela
                   </h1>
-                  <div className="inline-block bg-white px-10 py-4 mt-8 font-glori rounded-4xl text-2xl">
-                    <span>UI/UX Designer and Frontend Developer</span>
-                  </div>
+
+                  {/* Subtitle con SplitText */}
+                  <SplitText
+                    text="UI/UX Designer and Frontend Developer"
+                    className="inline-block bg-white px-10 py-4 mt-8 font-glori rounded-4xl text-2xl"
+                    delay={100}
+                    duration={2}
+                    ease="power3.out"
+                    splitType="chars"
+                    from={{ opacity: 0, y: 40 }}
+                    to={{ opacity: 1, y: 0 }}
+                    threshold={0.1}
+                    rootMargin="-100px"
+                    textAlign="center"
+                  />
                 </div>
               </SlideUp>
             </div>
